b8c927b4f9b80c5601d7ef8af7bbf566299b9aab
Vishal Sengar <vishalsengardtu@gmail.com>
2020-12-19 20:05:37 +0530

    Create and Insert Documents

--- src/app.js
+++ src/app.js
@@ -2,18 +2,16 @@ const mongoose = require('mongoose')
 
 
 // connecting to database
-
 mongoose.connect("mongodb://localhost:27017/Youtube",
     {
         useNewUrlParser: true,
         useUnifiedTopology: true
     })
     .then(() => console.log("Connecting to Database..."))
-    .catch(err => console.log(`Connection Error: ${err}`))
+    .catch(err => console.log(err))
 
 
 // schema - it defines the structure of the document
-
 const playlistSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -31,5 +29,26 @@ const playlistSchema = new mongoose.Schema({
 
 
 // model - it provides an interface to the database for performing CURD operations
-
 const Playlist = new mongoose.model("Playlist", playlistSchema)
+
+
+// create or insert document
+const createDocument = async () => {
+    try { 
+        const reactPlaylist = new Playlist({
+            name: "Node JS",
+            type: "Back End",
+            videos: 50,
+            author: "Vishal Sengar",
+            active: true,
+        })
+            
+        const result = await reactPlaylist.save()
+        console.log(result)
+    } catch (err) {
+        console.log(err)
+    } 
+}
+
+createDocument()
+
